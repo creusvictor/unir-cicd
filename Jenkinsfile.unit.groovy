28c8d527bb9f14f15e8bdd81d4eda394a08b8de0
creusvictor <victorcg98@gmail.com>
2023-06-06 07:02:09 +0200

    jenkinsfile modified

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -15,10 +15,30 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
+        stage('API tests') {
+            steps {
+                sh 'make test-api'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
+        stage('E2E tests') {
+            steps {
+                sh 'make test-e2e'
+                archiveArtifacts artifacts: 'results/*.xml'
+            }
+        }
     }
     post {
         always {
             junit 'results/*_result.xml'
+            cleanWs()
+        }
+        failure {
+            emailtext ( 
+                to: 'victor.creus874@comunidadunir.net',
+                subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
+                body: "The Job ${env.JOB_NAME} finished with failure. Plesea check the details."
+            )
         }
     }
 }
